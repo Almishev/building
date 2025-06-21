fb1226d77a1266a84bf71a3f27fc7aeafdc01e3f
Anton Almishev <antonalmishev@abv.bg>
2025-06-21 10:58:03 +0300

    Clean up config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/building',
-  assetPrefix: '/building/',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
