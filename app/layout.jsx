0cb239bcecf3b6dfed71d63f2250ccd377a315b5
Anton Almishev <antonalmishev@abv.bg>
2025-06-30 13:29:11 +0300

    add marker

--- app/layout.jsx
+++ app/layout.jsx
@@ -114,6 +114,16 @@ export default function RootLayout({ children }) {
           }}
         />
         <link rel="manifest" href="/manifest.json" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17054174704"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17054174704');
+          `
+        }} />
       </head>
       <body className={`${dmSans.variable} ${barlow.variable} antialiased`}>
         <Header />
